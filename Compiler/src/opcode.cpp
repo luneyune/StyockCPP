ee4b53d2da114988b87bc6dba666b675a9aea7f4
Ivan Kirspu <luneyune@users.noreply.github.com>
2023-03-07 20:53:07 +0700

    Add opcode mnemonics

---
+++ Compiler/src/opcode.cpp
@@ -0,0 +1,7 @@
+#include "opcode.hpp"
+
+
+const std::string OPCODES_MNEMONIC[] = {"stop", "jmp", "js", "jl", "jle", "jg", "jge", "je",
+                                        "readd", "readc", "reads", "printd", "printc", "prints",
+                                        "add", "sub", "div", "mod", "mul", "cmp",
+                                        "push", "pop"};
--- Compiler/src/opcode.hpp
+++ Compiler/src/opcode.hpp
@@ -1,11 +1,15 @@
 #ifndef STYOCKCPP_COMPILER_SRC_OPCODE_HPP_
 #define STYOCKCPP_COMPILER_SRC_OPCODE_HPP_
 
+#include <string>
+
 enum Opcode {
     STOP, JMP, JS, JL, JLE, JG, JGE, JE, // Programm control opcodes
     READD, READC, READS, PRINTD, PRINTC, PRINTS, // I/O opcodes
     ADD, SUB, DIV, MOD, MUL, CMP, // Math operations opcodes
     PUSH, POP // Data operation opcodes
 };
+
+extern const std::string OPCODES_MNEMONIC[];
 
 #endif
